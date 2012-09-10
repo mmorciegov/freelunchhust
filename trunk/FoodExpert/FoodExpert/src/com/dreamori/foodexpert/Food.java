650a1536f4e6fdf3a89fd2a763037352b8542064
FreeLunchHust <FreeLunchHust@gmail.com>
2012-09-10 07:52:18 +0000

    Sample for main ui
    by tom

--- trunk/FoodExpert/FoodExpert/src/com/dreamori/foodexpert/Food.java
+++ trunk/FoodExpert/FoodExpert/src/com/dreamori/foodexpert/Food.java
@@ -19,14 +19,14 @@ public class Food extends TitleActivity {
         List<GridViewHolderData> dataList = new ArrayList<GridViewHolderData>();
         
         GridViewHolderData inq_data = new GridViewHolderData();
-        inq_data.icon = R.drawable.food;
-        inq_data.name = getString(R.string.menu_food_search);
+        inq_data.icon = R.drawable.foodrelationship;
+        //inq_data.name = getString(R.string.menu_food_search);
         inq_data.degree = 0;
         dataList.add(inq_data);
         
         GridViewHolderData disease_data = new GridViewHolderData();
-        disease_data.icon = R.drawable.food;
-        disease_data.name = getString(R.string.menu_food_disease);
+        disease_data.icon = R.drawable.foodanddisease;
+        //disease_data.name = getString(R.string.menu_food_disease);
         disease_data.degree = 0;       
         dataList.add(disease_data);
         
