daabe206db38875c5cfcb78a344d3092b2a0ddb1
FreeLunchHust@gmail.com <FreeLunchHust@gmail.com@977cf84f-7ba5-d7ba-42f9-e4e37eaa4d2d>
2012-09-17 13:40:54 +0000

    Update code
    by tom

--- trunk/FoodExpert/FoodExpert/src/com/dreamori/foodexpert/ContentPage.java
+++ trunk/FoodExpert/FoodExpert/src/com/dreamori/foodexpert/ContentPage.java
@@ -16,6 +16,24 @@ public class ContentPage extends TitleActivity {
 	public ImageView m_imageViewDegree;
 	public TextView m_textViewHint;
 	
+	
+	
+	@Override
+	protected void onDestroy() {
+		super.onDestroy();
+		
+		m_textViewHint = null;
+		m_textView1 = null;
+		m_textView2 = null;		
+		m_imageView1 = null;
+		m_imageView2 = null;
+		m_imageViewDegree = null;
+		
+	}
+	
+
+
+
 	public void InitFood(ImageView image, TextView text, String name)
 	{
 		image.setBackgroundResource(ResourceManager.GetIcon(this, getDatabaseHelper().getIconName(name)));
