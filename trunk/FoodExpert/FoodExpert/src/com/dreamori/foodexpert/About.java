4e8bfa339a65941fd8d252559483e0e32e2fc2be
FreeLunchHust@gmail.com <FreeLunchHust@gmail.com@977cf84f-7ba5-d7ba-42f9-e4e37eaa4d2d>
2012-09-25 08:46:43 +0000

    add about activity update UI
    by alucardshi

---
+++ trunk/FoodExpert/FoodExpert/src/com/dreamori/foodexpert/About.java
@@ -0,0 +1,14 @@
+package com.dreamori.foodexpert;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class About extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about);
+    }
+}
