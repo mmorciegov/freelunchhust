84133808ba9dd94c11854f6943799dda06ca647e
FreeLunchHust@gmail.com <FreeLunchHust@gmail.com@977cf84f-7ba5-d7ba-42f9-e4e37eaa4d2d>
2013-09-02 06:12:05 +0000

    NO CR: Update Const.java by tom

--- trunk/DaoDeJing/src/com/dreamori/daodejing/Const.java
+++ trunk/DaoDeJing/src/com/dreamori/daodejing/Const.java
@@ -1,6 +1,6 @@
 package com.dreamori.daodejing;
 
 public final class Const {
-	static final  int m_minImageIndex = 0;
-	static final  int m_maxImageIndex = 73;
+	static final  int m_minImageIndex = 1;
+	static final  int m_maxImageIndex = 81;
 }
